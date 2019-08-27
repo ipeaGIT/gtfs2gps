2b8ab439be16a3d85936565f5d89074231781ce2
pedro-andrade-inpe <pedro.andrade@inpe.br>
2019-08-27 09:14:29 -0300

    Returning a list in read_gtfs().

--- R/read_gtfs.R
+++ R/read_gtfs.R
@@ -1,34 +1,36 @@
 
-#' Read files of a gtfs.zip feed and load them to memory as data.frames/data.tables. The 
-#' function will load to memory the following files: "calendar.txt", "routes.txt", "shapes.txt", 
-#' "stop_times.txt", "stops.txt" and "trips.txt".
-#'
-#' @param gtfszip A zipped Year of the data (defaults to 2010)
+#' @title Read GTFS data into a list of data.tables
+#' @description Read files of a zipped GTFS feed and load them to memory as a list of data.tables.
+#' It will load the following files: "calendar.txt", "routes.txt", "shapes.txt", 
+#' "stop_times.txt", "stops.txt", and "trips.txt". If some of these files do not exit,
+#' it will stop with an error.
+#' @param gtfszip A zipped GTFS data.
 #' @export
-#' @examples \donttest{
-#'
+#' @examples
 #' library(gtfs2gps)
 #'
-#' read_gtfs(gtfszip= "./data/poa_gtfs.zip")
-#'}
+#' poa <- read_gtfs(system.file("extdata/poa.zip", package="gtfs2gps"))
 read_gtfs <- function(gtfszip){
-# Use GForce Optimisations in data.table operations
+  # Use GForce Optimisations in data.table operations
   # details > https://jangorecki.gitlab.io/data.cube/library/data.table/html/datatable-optimize.html
-  options(datatable.optimize=Inf)
+  options(datatable.optimize = Inf)
 
-# Unzip files
+  # Unzip files
   tempd <- file.path(tempdir(), "gtfsdir") # create tempr dir to save GTFS unzipped files
   unlink(paste0(normalizePath(tempd), "/", dir(tempd)), recursive = TRUE) # clean tempfiles in that dir
-  unzip(zipfile = gtfszip, exdir = tempd, overwrite = T) # unzip files
+  unzip(zipfile = gtfszip, exdir = tempd, overwrite = TRUE) # unzip files
   unzippedfiles <- list.files(tempd) # list of unzipped files
 
-    
-# read files to memory
-  if("routes.txt" %in% unzippedfiles){     routes <<- data.table::fread( paste0(tempd,"/routes.txt"), encoding="UTF-8")}       else{stop(message("Error: File routes.txt is missing"))}
-  if("stops.txt" %in% unzippedfiles){      stops <<- data.table::fread( paste0(tempd,"/stops.txt"), encoding="UTF-8")}         else{stop(message("Error: File stops.txt is missing"))}
-  if("stop_times.txt" %in% unzippedfiles){ stoptimes <<- data.table::fread( paste0(tempd,"/stop_times.txt"), encoding="UTF-8")}else{stop(message("Error: File stop_times.txt is missing"))}
-  if("shapes.txt" %in% unzippedfiles){     shapes <<- data.table::fread( paste0(tempd,"/shapes.txt"), encoding="UTF-8")}       else{stop(message("Error: File shapes.txt is missing"))}
-  if("trips.txt" %in% unzippedfiles){      trips <<- data.table::fread( paste0(tempd,"/trips.txt"), encoding="UTF-8")}         else{stop(message("Error: File trips.txt is missing"))}
-  if("calendar.txt" %in% unzippedfiles){   calendar <<- data.table::fread( paste0(tempd,"/calendar.txt"), encoding="UTF-8")}   else{stop(message("Error: File calendar.txt is missing"))}
-  if("frequencies.txt" %in% unzippedfiles){frequencies <<- data.table::fread( paste0(tempd,"/frequencies.txt"), encoding="UTF-8") }
+  result <- list()
+
+  # read files to memory
+  if("routes.txt"      %in% unzippedfiles){result$routes      <- data.table::fread(paste0(tempd,"/routes.txt"),      encoding="UTF-8")}  else{stop(message("Error: File routes.txt is missing"))}
+  if("stops.txt"       %in% unzippedfiles){result$stops       <- data.table::fread(paste0(tempd,"/stops.txt"),       encoding="UTF-8")}  else{stop(message("Error: File stops.txt is missing"))}
+  if("stop_times.txt"  %in% unzippedfiles){result$stoptimes   <- data.table::fread(paste0(tempd,"/stop_times.txt"),  encoding="UTF-8")}  else{stop(message("Error: File stop_times.txt is missing"))}
+  if("shapes.txt"      %in% unzippedfiles){result$shapes      <- data.table::fread(paste0(tempd,"/shapes.txt"),      encoding="UTF-8")}  else{stop(message("Error: File shapes.txt is missing"))}
+  if("trips.txt"       %in% unzippedfiles){result$trips       <- data.table::fread(paste0(tempd,"/trips.txt"),       encoding="UTF-8")}  else{stop(message("Error: File trips.txt is missing"))}
+  if("calendar.txt"    %in% unzippedfiles){result$calendar    <- data.table::fread(paste0(tempd,"/calendar.txt"),    encoding="UTF-8")}  else{stop(message("Error: File calendar.txt is missing"))}
+  if("frequencies.txt" %in% unzippedfiles){result$frequencies <- data.table::fread(paste0(tempd,"/frequencies.txt"), encoding="UTF-8")}
+
+  return(result)
 }
