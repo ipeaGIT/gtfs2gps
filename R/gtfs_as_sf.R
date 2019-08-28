ab575b9a52d451bf63527f64e08c72c6a35ea226
pedro-andrade-inpe <pedro.andrade@inpe.br>
2019-08-28 12:13:33 -0300

    Function to convert stops to sf format.

--- R/gtfs_as_sf.R
+++ R/gtfs_as_sf.R
@@ -27,3 +27,21 @@ gtfs_shapes_as_sf <- function(gtfs, crs = 4326){
     stringsAsFactors = FALSE) %>%
     sf::st_sf(crs = crs)
 }
+
+#' @title Convert GTFS stops to simple feature
+#' @description Convert a GTFS stops data loaded using gtfs2gps::read_gtf()
+#' into a point simple feature (sf).
+#' @param gtfs A GTFS data.
+#' @param crs The coordinate reference system represented as an EPSG code.
+#' The default value is 4326 (latlong WGS84)
+#' @export
+#' @examples
+#' poa <- read_gtfs(system.file("extdata/poa.zip", package="gtfs2gps"))
+#' poa_shapes <- gtfs_shapes_as_sf(poa)
+#' poa_stops <- gtfs_stops_as_sf(poa)
+#' plot(st_geometry(poa_shapes), lwd = 2)
+#' plot(st_geometry(poa_stops), pch = 20, col ="blue", add = TRUE)
+gtfs_stops_as_sf <- function(gtfs, crs = 4326){
+  gtfs$stops %>%
+    sf::st_as_sf(coords = c('stop_lon', 'stop_lat'), agr = "identity", crs = crs)
+}
--- tests/testthat/test_gtfs_as_sf.R
+++ tests/testthat/test_gtfs_as_sf.R
@@ -8,4 +8,12 @@ test_that("gtfs_shape_as_sf", {
     expect_true(inherits(poa_sf, "sf"))
     expect_equal(dim(poa_sf)[1], 4)
 })
+
+test_that("gtfs_stops_as_sf", {
+  poa <- read_gtfs(system.file("extdata/poa.zip", package="gtfs2gps"))
+  
+  poa_sf <- gtfs_stops_as_sf(poa)
+  
+  expect_true(inherits(poa_sf, "sf"))
+  expect_equal(dim(poa_sf)[1], 212)
 })
