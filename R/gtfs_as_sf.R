f2bbcfd348cbff339cfb2e71b74f81f658f353ca
rafapereirabr <rafa.pereira.br@gmail.com>
2019-09-01 09:50:12 -0300

    fixed typo

--- R/gtfs_as_sf.R
+++ R/gtfs_as_sf.R
@@ -12,7 +12,7 @@
 #' plot(poa_sf["id"], lwd = 2)
 gtfs_shapes_as_sf <- function(gtfs, crs = 4326){
   
-  temp_shapes <- g$shapes[,
+  temp_shapes <- gtfs$shapes[,
                           {
                             geometry <- sf::st_linestring(x = matrix(c(shape_pt_lon, shape_pt_lat), ncol = 2))
                             geometry <- sf::st_sfc(geometry)
