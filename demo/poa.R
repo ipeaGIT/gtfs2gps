d9750ec6c2772add2ba1e327e39e02f8b2522ad9
pedro-andrade-inpe <pedro.andrade@inpe.br>
2019-09-04 16:48:13 -0300

    Moving R scripts not yet organized as functions to demo.

--- demo/poa.R
+++
@@ -1,7 +0,0 @@
-
-poa <- tidytransit::read_gtfs(system.file("extdata/poa.zip", package="gtfs2gps"),
-                       local = TRUE,
-                       geometry = TRUE,
-                       frequency = TRUE)
-
-plot(poa)
