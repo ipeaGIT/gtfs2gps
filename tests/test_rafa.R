19fba5f297a87801439acc09b424ad1240f201fa
rafapereirabr <rafa.pereira.br@gmail.com>
2019-09-22 15:16:28 -0300

    removing igraph test to `test` directory

--- tests/test_rafa.R
+++ tests/test_rafa.R
@@ -59,3 +59,93 @@ p <-   profvis( update_newstoptimes("T2-1@1#2146") )
 p <-   profvis( b <- corefun("T2-1") )
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+### update package documentation ----------------
+# http://r-pkgs.had.co.nz/release.html#release-check
+
+
+rm(list = ls())
+
+library(roxygen2)
+library(devtools)
+library(usethis)
+
+
+
+
+setwd("R:/Dropbox/git/gtfs2gps")
+setwd("..")
+
+# update `NEWS.md` file
+# update `DESCRIPTION` file
+# update ``cran-comments.md` file
+
+
+# checks spelling
+library(spelling)
+devtools::spell_check(pkg = "gtfs2gps", vignettes = TRUE, use_wordlist = TRUE)
+
+# Update documentation
+devtools::document(pkg = "gtfs2gps")
+
+
+# Write package manual.pdf
+system("R CMD Rd2pdf --title=Package geobr --output=./geobr/manual.pdf")
+# system("R CMD Rd2pdf geobr")
+
+
+
+
+# Ignore these files/folders when building the package (but keep them on github)
+setwd("R:/Dropbox/git_projects/geobr")
+
+
+usethis::use_build_ignore("test")
+usethis::use_build_ignore("prep_data")
+usethis::use_build_ignore("manual.pdf")
+
+# script da base de dados e a propria base armazenada localmente, mas que eh muito grande para o CRAN
+usethis::use_build_ignore("brazil_2010.R")
+usethis::use_build_ignore("brazil_2010.RData")
+usethis::use_build_ignore("brazil_2010.Rd")
+
+# Vignette que ainda nao esta pronta
+usethis::use_build_ignore("  Georeferencing-gain.R")
+usethis::use_build_ignore("  Georeferencing-gain.Rmd")
+
+# temp files
+usethis::use_build_ignore("crosswalk_pre.R")
+
+
+
+
+setwd("R:/Dropbox/git_projects/geobr")
+setwd("..")
+
+
+# Install package
+# devtools::install("geobr", build_vignettes = T)
+# system("R CMD INSTALL --build geobr")
+
+# build binary
+system("R CMD build geobr --resave-data") # build tar.gz
+# devtools::build(pkg = "geobr", path=".", binary = T, manual=T)
+
+# Check package errors
+# devtools::check("geobr")
+system("R CMD check geobr_1.0.tar.gz")
+system("R CMD check --as-cran geobr_1.0.tar.gz")
+
