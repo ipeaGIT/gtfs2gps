9accc65ccc1fb678c7342b276e2f21cfa93a6a77
pedro-andrade-inpe <pedro.andrade@inpe.br>
2019-10-24 10:53:19 -0300

    Close #33. Update cpp_snap_points to have a limit of recursive calls.

--- src/snap_points.cpp
+++ src/snap_points.cpp
@@ -7,6 +7,48 @@ double distanceHaversine(double latf, double lonf, double latt, double lont,
                          double tolerance);
 double toRadians(double deg);
 
+Rcpp::NumericVector cpp_snap_points_level(Rcpp::NumericMatrix& data, Rcpp::NumericMatrix& ref, int spatial_resolution, int level){
+  if(level > 4){
+    double max_dist = spatial_resolution ^ level; 
+
+    std::stringstream text;
+    text << "Could not find a nearest point closer than " << max_dist << "m.";
+    stop(text.str());
+  }
+
+  Rcpp::NumericVector result_pos;
+  
+  const int nrow = data.nrow();
+  const int ref_nrow = ref.nrow();
+  int ref_i = -1;
+  int total_found = 0;
+  
+  for(int i = 0; i < nrow; i++){
+    const double x = data[i];
+    const double y = data[i + nrow];
+    double dist;
+    
+    do {
+      ref_i++;
+      const double ref_x = ref[ref_i];
+      const double ref_y = ref[ref_i + ref_nrow];
+      
+      dist = distanceHaversine(toRadians(y), toRadians(x), toRadians(ref_y), toRadians(ref_x), 1);
+    } while (dist > spatial_resolution && ref_i < ref_nrow);
+    
+    if(ref_i < ref_nrow){
+      result_pos.push_back(ref_i + 1);
+      total_found++;
+    }
+  }
+  
+  if(total_found < nrow){
+    return cpp_snap_points_level(data, ref, spatial_resolution * 2, level + 1);
+  }
+
+  return result_pos;
+}
+
 //' @title Snap points to the closest points from another set
 //' @description Snap a set of points to the closest points available in another set
 //' of points. The result is a data.frame.
@@ -21,35 +63,5 @@ double toRadians(double deg);
 //' @export
 // [[Rcpp::export]]
 Rcpp::NumericVector cpp_snap_points(Rcpp::NumericMatrix& data, Rcpp::NumericMatrix& ref, int spatial_resolution){
-  Rcpp::NumericVector result_pos;
-  
-  const int nrow = data.nrow();
-  const int ref_nrow = ref.nrow();
-  int ref_i = -1;
-  int total_found = 0;
-
-  for(int i = 0; i < nrow; i++){
-    const double x = data[i];
-    const double y = data[i + nrow];
-    double dist;
-
-    do {
-      ref_i++;
-      const double ref_x = ref[ref_i];
-      const double ref_y = ref[ref_i + ref_nrow];
-
-      dist = distanceHaversine(toRadians(y), toRadians(x), toRadians(ref_y), toRadians(ref_x), 1);
-    } while (dist > spatial_resolution && ref_i < ref_nrow);
-
-    if(ref_i < ref_nrow){
-      result_pos.push_back(ref_i + 1);
-      total_found++;
-    }
-  }
-
-  if(total_found < nrow){
-    return cpp_snap_points(data, ref, spatial_resolution * 2);
-  }
-
-  return result_pos;
+  return cpp_snap_points_level(data, ref, spatial_resolution, 1);
 }
