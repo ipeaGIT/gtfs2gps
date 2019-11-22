7f43f87394fd79386f258b543cf993a1758699bb
pedro-andrade-inpe <pedro.andrade@inpe.br>
2019-11-22 16:03:18 -0300

    Removing older gtfs2gps functions that were put together. #3.

--- tests/testthat/test_gps_as_sf.R
+++ tests/testthat/test_gps_as_sf.R
@@ -3,7 +3,7 @@ context("gps_as_sf")
 test_that("gps_as_sf", {
   poa <- system.file("extdata/poa.zip", package="gtfs2gps")
   
-  poa_gps_sf <- gps_as_sf(gtfs2gps_dt_parallel(poa, progress = FALSE))
+  poa_gps_sf <- gps_as_sf(gtfs2gps(poa, progress = FALSE))
   
   expect_true(is(poa_gps_sf, "sf"))
 })
