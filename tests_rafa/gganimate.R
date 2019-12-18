ed400910bc8ebf19865998290f02e3a29038f293
rafapereirabr <rafa.pereira.br@gmail.com>
2019-12-18 16:56:11 -0300

    tests gganimate

--- tests_rafa/gganimate.R
+++ tests_rafa/gganimate.R
@@ -6,6 +6,7 @@ library(data.table)
 library(ggthemes)
 
 
+
 # Generate GPS-like data
 poa <- system.file("extdata/poa.zip", package="gtfs2gps")
 poa_gps <- gtfs2gps(poa, progress = FALSE)
@@ -21,7 +22,7 @@ ggplot() + geom_sf(data=network_sf, aes(color=shape_id) )
 # dt <- subset(poa_gps, trip_id %in% c('176-1@1#1028' , 'A141-1@1#1750', 'R10-2@1#1010'))
 # dt <- subset(poa_gps, trip_id %in% unique(poa_gps$trip_id)[1:10] )
 # dt <- subset(poa_gps, trip_id %in% c('176-1@1#1028' , 'A141-1@1#1750', 'R10-2@1#1010'))
-dt <- poa_gps[ between(departure_time, lower = as.ITime("07:00:00"), upper = as.ITime("07:30:00")) ]
+dt <- poa_gps[ between(departure_time, lower = as.ITime("07:00:00"), upper = as.ITime("12:30:00")) ]
 
 
 
@@ -79,13 +80,12 @@ https://www.datanovia.com/en/blog/gganimate-how-to-create-plots-with-beautiful-a
 animation_r <- 
 
 ggplot() +
-    geom_path(data=dt, aes(color = trip_id,  x=shape_pt_lon, y=shape_pt_lat), show.legend = FALSE, alpha = 0.7 ) +
+    geom_path(data=dt, aes(color = trip_id,  x=shape_pt_lon, y=shape_pt_lat), show.legend = FALSE, alpha = 0.2 ) +
     # geom_point(show.legend = FALSE, alpha = 0.7) +
     scale_color_viridis_d() +
     # Here comes the gganimate specific bits
-    labs(title = 'Time: {frame_time}') +
     transition_reveal(as.POSIXct(departure_time)) +
-    ease_aes('linear') +
+    # ease_aes('linear') +
     theme_map()
 
   
