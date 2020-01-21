81d1ce1a08d510aa7842d8ae5275fbcc22e243cc
rafapereirabr <rafa.pereira.br@gmail.com>
2020-01-21 15:08:13 -0300

    improve speed of  `gtfs_shapes_as_sf` closes #80

--- R/gtfs_as_sf.R
+++ R/gtfs_as_sf.R
@@ -7,26 +7,27 @@
 #' The default value is 4326 (latlong WGS84)
 #' @export
 #' @examples \donttest{
-#' poa <- read_gtfs(system.file("extdata/poa.zip", package="gtfs2gps"))
+#' poa <- read_gtfs(system.file("extdata/saopaulo.zip", package="gtfs2gps"))
 #' poa_sf <- gtfs_shapes_as_sf(poa)
 #' plot(sf::st_geometry(poa_sf), lwd = 2)
 #' }
 gtfs_shapes_as_sf <- function(gtfs, crs = 4326){
-  temp_shapes <- setDT(gtfs$shapes)[,
-                          {
-                            geometry <- sf::st_linestring(x = matrix(c(shape_pt_lon, shape_pt_lat), ncol = 2))
-                            geometry <- sf::st_sfc(geometry)
-                            geometry <- sf::st_sf(geometry = geometry)
-                          }
-                          , by = shape_id
-                          ]
-
-  temp_shapes <- sf::st_as_sf(temp_shapes, crs = crs)
-  setDT(temp_shapes)[, length := sf::st_length(geometry) %>% units::set_units("km") ] 
+  # sort data
+  temp_shapes <- data.table::setDT(gtfs$shapes)[order(shape_id, shape_pt_sequence)]
+  
+  # convert to sf
+  temp_shapes <- sfheaders::sf_linestring(temp_shapes, x = "shape_pt_lon" , y = "shape_pt_lat", linestring_id = "shape_id")
+  
+  # add projection
+  st_crs(temp_shapes) <- crs
+  
+  # calculate distances
+  data.table::setDT(temp_shapes)[, length := sf::st_length(geometry) %>% units::set_units("km") ] 
+  
+  # back to sf
   temp_shapes <- sf::st_sf(temp_shapes)
   return(temp_shapes)
 }
-
 
 #' @title Convert GTFS stops to simple feature object
 #' @description Convert a GTFS stops data loaded using gtfs2gps::read_gtf()
@@ -43,6 +44,7 @@ gtfs_shapes_as_sf <- function(gtfs, crs = 4326){
 #' plot(sf::st_geometry(poa_stops), pch = 20, col ="blue", add = TRUE)
 #' }
 gtfs_stops_as_sf <- function(gtfs, crs = 4326){
-  gtfs$stops %>%
-    sf::st_as_sf(coords = c('stop_lon', 'stop_lat'), agr = "identity", crs = crs)
+  temp_stops_sf <- sfheaders::sf_point(gtfs$stops, x = "stop_lon", y="stop_lat", keep = T)
+  st_crs(temp_stops_sf) <- 4326
+  return(temp_stops_sf)
 }
