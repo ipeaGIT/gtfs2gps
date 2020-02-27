482dcec70dc5eaa5ccf63be9b6eb599662f39386
pedro-andrade-inpe <pedro.andrade@inpe.br>
2020-02-27 09:49:56 -0300

    Adding example to gps_as_sf().

--- R/gps_as_sf.R
+++ R/gps_as_sf.R
@@ -4,6 +4,15 @@
 #' @param gps A data.table with timestamp data.
 #' @param crs A Coordinate Reference System. The default value is 4326 (latlong WGS84)
 #' @export
+#' @examples
+#' library(gtfs2gps)
+#' library(dplyr)
+#' 
+#' fortaleza <- read_gtfs(system.file("extdata/fortaleza.zip", package="gtfs2gps"))
+#' ids <- fortaleza$shapes$shape_id %>% unique()
+#' fortaleza <- filter_by_shape_id(fortaleza, ids[1])
+#' for_gps <- gtfs2gps(fortaleza)
+#' for_gps_sf <- gps_as_sf(for_gps)
 gps_as_sf <- function(gps, crs = 4326){
   # convert to sf
   temp_gps <- sfheaders::sf_multipoint(gps, x = "shape_pt_lon", y = "shape_pt_lat",
