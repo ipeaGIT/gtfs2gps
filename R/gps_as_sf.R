896f493da73e60e4e61b9dc64546dcf25d6c0f66
pedro-andrade-inpe <pedro.andrade@inpe.br>
2020-04-09 22:48:57 -0300

    Fixing small typo.

--- R/gps_as_sf.R
+++ R/gps_as_sf.R
@@ -2,7 +2,7 @@
 #' @title Convert GPS-like data.table to a Simple Feature object
 #' @description Convert a GPS data stored in a data.table into a Simple Feature.
 #' @param gps A data.table with timestamp data.
-#' @param crs A Coordinate Reference System. The default value is 4326 (latlong WGS84)
+#' @param crs A Coordinate Reference System. The default value is 4326 (latlong WGS84).
 #' @return A simple feature (sf) object.
 #' @export
 #' @examples
