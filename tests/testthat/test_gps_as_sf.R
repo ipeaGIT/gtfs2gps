8e456b34759b189aea1d2e4cbe3bb9ce6e494554
pedro-andrade-inpe <pedro.andrade@inpe.br>
2020-04-23 11:57:13 -0300

    Close #115 adding height to points. Also related to #114.

--- tests/testthat/test_gps_as_sf.R
+++ tests/testthat/test_gps_as_sf.R
@@ -5,3 +5,26 @@ test_that("gps_as_sf", {
   
   expect_true(is(poa_gps_sf, "sf"))
 })
+
+
+test_that("gps_as_sf_points", {
+  fortaleza <- read_gtfs(system.file("extdata/fortaleza.zip", package = "gtfs2gps"))
+  srtmfile <- system.file("extdata/fortaleza-srtm.tif", package="gtfs2gps")
+  
+  subset <- fortaleza %>%
+    filter_week_days() %>%
+    filter_single_trip() %>%
+    filter_by_shape_id(c("shape804-I", "shape806-I"))
+  
+  for_gps <- gtfs2gps(subset)
+  for_gps_sf_points <- gps_as_sf_points(for_gps) # without height
+  coords <- for_gps_sf_points %>% sf::st_coordinates()
+  
+  expect_equal(dim(coords)[2], 2)
+    
+  for_gps <- append_height(for_gps, srtmfile)
+  for_gps_sf_points <- gps_as_sf_points(for_gps) # with height
+  coords <- for_gps_sf_points %>% sf::st_coordinates()
+  
+  expect_equal(dim(coords)[2], 3)
+})
