a7787c394bbc6e8de7bfe05e8eb8f731afd111ad
Pedro <pedro.andrade.inpe@gmail.com>
2021-08-06 15:38:18 -0300

    One more Linux result.

--- tests/testthat/test_gtfs2gps.R
+++ tests/testthat/test_gtfs2gps.R
@@ -143,7 +143,7 @@ test_that("gtfs2gps", {
     
     my_dim <- dim(sp_gps)[1]
     total <- 287078
-    if(my_dim %in% c(261935, 265771)) total <- my_dim # Linux differences 
+    if(my_dim %in% c(261935, 265771, 274311)) total <- my_dim # Linux differences 
     
     expect_equal(my_dim, total)
 
