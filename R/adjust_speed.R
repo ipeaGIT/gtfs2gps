63eb41fcc96633a0ae70d21dc482240aeb881497
Pedro <pedro.andrade.inpe@gmail.com>
2021-10-13 09:24:35 -0300

    Estimating departure time before first stop and after last stop. Close #206

--- R/adjust_speed.R
+++ R/adjust_speed.R
@@ -29,13 +29,15 @@
 #' poa_gps <- gtfs2gps(poa)
 #' poa_gps_new <- adjust_speed(poa_gps)
 adjust_speed <- function(gps_data, min_speed = 2, max_speed = 80, new_speed = NULL){
+  gps_data <- data.table::copy(gps_data)
+
   max_speed <- units::set_units(max_speed, "km/h") %>% 
     units::set_units("m/s") %>%
-    as.numeric()
+    units::drop_units()
   
   min_speed <- units::set_units(min_speed, "km/h") %>%
     units::set_units("m/s") %>%
-    as.numeric()
+    units::drop_units()
 
   gps_data[, speed := units::drop_units(units::set_units(speed, "m/s"))]
   gps_data[speed == "Inf" | is.na(speed) | is.nan(speed), speed := NA]
@@ -46,22 +48,60 @@ adjust_speed <- function(gps_data, min_speed = 2, max_speed = 80, new_speed = NU
   else
     new_speed <- units::set_units(new_speed, "km/h") %>%
       units::set_units("m/s") %>%
-      as.numeric()
+      units::drop_units()
 
+  gps_data[, dist := units::drop_units(dist)]
+  gps_data[, cumdist := units::drop_units(cumdist)]
+  gps_data[, cumtime := units::drop_units(cumtime)]
+  
   gps_data[is.na(speed), speed := new_speed]
-  gps_data[, speed := units::set_units(speed, "m/s")]
-  gps_data[, speed := units::set_units(speed, "km/h")]
 
-  gps_data[, time := units::set_units(dist / speed, "s")]
+  gps_data[, time := dist / speed]
   gps_data[, cumtime := cumsum(time), by = .(shape_id, trip_id, trip_number)]
+  
+  gps_data <- lapply(unique(gps_data$shape_id), function(i){
+    tmp_gps_data <- gps_data[shape_id == i, ]
+    
+    if(nrow(tmp_gps_data[!is.na(departure_time)]) < 2){
+     message(paste0("departure_time column of shape_id ", i, " was not adjusted as there are less than two departure_time's."))
+     return(tmp_gps_data)
+    }
+    
+    id_first <- which(!is.na(tmp_gps_data$departure_time))[1]
+    
+    if(length(id_first) == 0){
+      message(paste0("departure_time column of shape_id ", i, " was not adjusted as there is no valid departure_time's."))
+      return(tmp_gps_data)
+    }
+    else if(id_first == 1){
+      tmp_gps_data[, 
+        departure_time := round(departure_time[1] + cumtime),
+        by = .(shape_id, trip_id, trip_number)]
+    }else{
+      # before the first valid observation
+      tmp_gps_data[1:id_first,
+        departure_time := round(departure_time[id_first] - cumtime),
+        by = .(shape_id, trip_id, trip_number)]
+     
+      # after the first valid observation
+      tmp_gps_data[id_first:.N,
+        departure_time := round(departure_time[id_first] + cumtime),
+        by = .(shape_id, trip_id, trip_number)]
+    }
+    
+    # midnight trips fix
+    tmp_gps_data[as.numeric(departure_time) > 86400, departure_time := departure_time - 86400]
+    
+    return(tmp_gps_data)
+  }) %>% data.table::rbindlist()
 
-  gps_data[, departure_time := as.numeric(departure_time)]
-  gps_data[, departure_time := units::set_units(departure_time, "s")]
-  gps_data[, departure_time := data.table::first(departure_time) + cumtime, by = .(shape_id, trip_id, trip_number)]
-  
-  gps_data[, departure_time := units::drop_units(departure_time)]
+  gps_data[, speed := units::set_units(units::set_units(speed, "m/s"), "km/h")]
+  gps_data[, dist := units::set_units(dist, "m")]
+  gps_data[, cumdist := units::set_units(cumdist, "m")]
+  gps_data[, time := units::set_units(time, "s")]
+  gps_data[, cumtime := units::set_units(cumtime, "s")]
+
   gps_data[, departure_time := data.table::as.ITime(departure_time)]
-  gps_data[, time := NULL]
-  
+
   return(gps_data)
 }
