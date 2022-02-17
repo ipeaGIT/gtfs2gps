63257005511f938d1855458be638bec2cd4c39d5
Pedro <pedro.andrade.inpe@gmail.com>
2022-02-17 15:07:02 -0300

    Close #227 Dropping units in the tests.

--- tests/testthat/test_adjust_speed.R
+++ tests/testthat/test_adjust_speed.R
@@ -8,22 +8,22 @@ test_that("adjust_speed", {
     expect_true(any(is.na(poa_gps$speed)))
     expect_true(any(is.na(poa_gps$cumtime)))
     
-    expect_equal(mean(poa_gps$speed, na.rm = TRUE), 24.7198, 0.0001)
-    expect_equal(mean(poa_gps$cumtime, na.rm = TRUE), 1588.508, 0.001)    
+    expect_equal(mean(units::drop_units(poa_gps$speed), na.rm = TRUE), 24.7198, 0.0001)
+    expect_equal(mean(units::drop_units(poa_gps$cumtime), na.rm = TRUE), 1588.508, 0.001)    
 
     poa_gps_new <- adjust_speed(poa_gps)
     
     expect_true(!any(is.na(poa_gps_new$speed)))
     expect_true(!any(is.na(poa_gps_new$cumtime)))
     
-    expect_equal(mean(poa_gps_new$speed), 25.05756, 0.0001)
-    expect_equal(mean(poa_gps_new$cumtime), 1599.597, 0.001)
+    expect_equal(mean(units::drop_units(poa_gps_new$speed)), 25.05756, 0.0001)
+    expect_equal(mean(units::drop_units(poa_gps_new$cumtime)), 1599.597, 0.001)
 
     poa_gps_new <- adjust_speed(poa_gps, min_speed = 25, max_speed = 50)
     
     expect_true(all(poa_gps_new$speed >= units::set_units(25, "km/h"), na.rm = TRUE))
     expect_true(all(poa_gps_new$speed <= units::set_units(50, "km/h"), na.rm = TRUE))
 
-    expect_equal(mean(poa_gps_new$speed, na.rm = TRUE), 35.09653, 0.0001)
-    expect_equal(mean(poa_gps_new$cumtime), 1131.914, 0.001)
+    expect_equal(mean(units::drop_units(poa_gps_new$speed), na.rm = TRUE), 35.09653, 0.0001)
+    expect_equal(mean(units::drop_units(poa_gps_new$cumtime)), 1131.914, 0.001)
 })
