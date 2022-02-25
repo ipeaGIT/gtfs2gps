35d821d19f66ea3e36102774e1c5be1da02e5d79
Joao Bazzo <joao.bazzo@gmail.com>
2022-02-25 15:33:38 -0300

    remove rgeos dependency and fix simplify_shapes() (Close #233)

--- tests/testthat/test_simplify_shapes.R
+++ tests/testthat/test_simplify_shapes.R
@@ -3,15 +3,15 @@ test_that("simplify_shapes", {
 
     expect_equal(poa$shapes$shape_id %>% unique() %>% length(), 4)
     expect_equal(dim(poa$shapes)[1], 1265)
-    
+
     poa_simpl <- simplify_shapes(poa, 1e-5)
-    
+
     expect(poa_simpl$shapes$shape_id %>% unique() %>% length(), 4)
-    expect_equal(dim(poa_simpl$shapes)[1], 859)
-    
+    expect_equal(dim(poa_simpl$shapes)[1], 867)
+
     poa_simpl <- simplify_shapes(poa, 1e-3)
-    
+
     expect(poa_simpl$shapes$shape_id %>% unique() %>% length(), 4)
-    expect_equal(dim(poa_simpl$shapes)[1], 98)
+    expect_equal(dim(poa_simpl$shapes)[1], 115)
 })
 
